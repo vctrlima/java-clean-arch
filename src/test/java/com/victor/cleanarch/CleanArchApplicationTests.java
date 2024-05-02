48e816acfdaf57d9d88de6c429753cfa86d9f75c
Victor Lima <victor.limac@icloud.com>
2024-05-02 12:37:59 -0300

    chore: initial commit

---
+++ src/test/java/com/victor/cleanarch/CleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.cleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
