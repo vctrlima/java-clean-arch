fc2072d3815f5c1c5af52943687cb538bd54dca0
Victor Lima <victor.limac@icloud.com>
2024-05-02 19:10:18 -0300

    fix: add existence validation on delete use-case

--- src/main/java/com/victor/cleanarch/data/usecase/DeleteCustomerByIdUseCaseImpl.java
+++ src/main/java/com/victor/cleanarch/data/usecase/DeleteCustomerByIdUseCaseImpl.java
@@ -2,6 +2,7 @@ package com.victor.cleanarch.data.usecase;
 
 import com.victor.cleanarch.data.protocols.db.DeleteCustomerByIdRepository;
 import com.victor.cleanarch.domain.usecase.DeleteCustomerByIdUseCase;
+import com.victor.cleanarch.domain.usecase.FindCustomerByIdUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +12,12 @@ public class DeleteCustomerByIdUseCaseImpl implements DeleteCustomerByIdUseCase 
     @Autowired
     private DeleteCustomerByIdRepository deleteCustomerByIdRepository;
 
+    @Autowired
+    private FindCustomerByIdUseCase findCustomerByIdUseCase;
+
     @Override
     public void deleteById(String id) {
+        findCustomerByIdUseCase.findById(id);
         deleteCustomerByIdRepository.deleteById(id);
     }
 
